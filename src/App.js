54c3e7eaf56d450d62145e54076e5ddcbe80e2b3
matvey033 <mat.gol20003@gmail.com>
2023-07-05 08:30:24 +0300

    Коммит проекта 27 урок

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Messages from './components/Messages/Messages';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Setting from './components/Setting/Setting';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/" element={<Profile postData={props.postData} />} />
+            <Route path="/profile/*" element={<Profile postData={props.postData} />} />
+            <Route path="/messages/*" element={<Messages dialogsData={props.dialogsData} messageData={props.messageData}/>} />
+            <Route path="/news/*" element={<News />} />
+            <Route path="/music/*" element={<Music />} />
+            <Route path="/setting/*" element={<Setting />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+let postData = [
+  { message: 'Hi, how are you?', count: '13' },
+  { message: 'What did you do in this week?', count: '19' },
+  { message: 'I wait your answers', count: '8' },
+]
+
+let dialogsData = [
+  { id: 1, name: 'Igor' },
+  { id: 2, name: 'Sveta' },
+  { id: 3, name: 'Dima' },
+  { id: 4, name: 'Lera' }
+]
+
+let messageData = [
+  { message: 'Hi' },
+  { message: 'Hello' },
+  { message: 'How are you?' }
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App postData={postData} dialogsData={dialogsData} messageData={messageData} />
   </React.StrictMode>
 );
 
