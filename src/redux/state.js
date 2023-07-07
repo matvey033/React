41b650395b2c8e8eae5907b8540092cf27c8155b
matvey033 <mat.gol20003@gmail.com>
2023-07-07 14:24:37 +0300

    lesson 29 add-ly

---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+let state = {
+    profilePage: {
+        postData: [
+            { message: 'Hi, how are you?', count: '13' },
+            { message: 'What did you do in this week?', count: '19' },
+            { message: 'I wait your answers', count: '8' },
+          ]
+    },
+
+    messagesPage: {
+        dialogsData: [
+            { id: 1, name: 'Igor' },
+            { id: 2, name: 'Sveta' },
+            { id: 3, name: 'Dima' },
+            { id: 4, name: 'Lera' },
+          ],
+          
+          messageData: [
+            { message: 'Hi' },
+            { message: 'Hello' },
+            { message: 'How are you?' },
+          ]
+    },
+    sidebar: {}
+}
+
+export default state;
